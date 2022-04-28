272071d3dde4fb59d6ace1358ede93cb28d67de1
Scot Gabriel <scot.gabriel@gmail.com>
2022-04-27 22:22:52 -0500

    .

--- file-get-fileVersion.ps1
+++ file-get-fileVersion.ps1
@@ -1,6 +1,6 @@
 $filePath = "C:\Path-To-file.exe"
 
-(Get-Item $filePath).VersionInfo | Select-Object FileVersion
+$fileVersion = (Get-Item $filePath).VersionInfo | Select-Object FileVersion
 
 if ($fileVersion -match "1.5.1.0") {
     Write-Host "The file is the right version"
