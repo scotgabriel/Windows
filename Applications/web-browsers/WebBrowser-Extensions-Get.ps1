d4a20c286c8e1aaf2496e9660fa628ceed5b20fe
Scot Gabriel <scot.gabriel@gmail.com>
2022-04-28 21:11:51 -0500

    WebBrowser Extensions Edge

--- Applications/web-browsers/WebBrowser-Extensions-Get.ps1
+++ Applications/web-browsers/WebBrowser-Extensions-Get.ps1
@@ -1,24 +1,29 @@
 [string]$OsDrive = (Get-WmiObject Win32_OperatingSystem).SystemDrive
-write-host "Operating System is on drive: " $OsDrive
 
 $UserProfiles = Get-ChildItem -Path "$OsDrive\Users"
-foreach ($user in $UserProfiles){
-    Write-Host "User Profile: " $user
-}
 
 $EdgeUserDataPart = "AppData\Local\Microsoft\Edge\User Data"
 
 Function EdgeBrowserProfiles($EdgeUser) {
     $EdgeUserData = ''
     [string]$EdgeUserData = $OsDrive + '\Users\' + $EdgeUser + '\' + $EdgeUserDataPart
-    # Write-Host "Base Edge Path for this user: "$EdgeUserData
-    if ($EdgeUserData | Test-Path){
+    if ($EdgeUserData | Test-Path) {
         $EdgeUserDataPossibleProfiles = Get-ChildItem -Path $EdgeUserData
-        foreach ($Folder in $EdgeUserDataPossibleProfiles){
-            if ($Folder -eq "Default" -or $Folder -like "Profile*"){
-                # Write-Host "Edge Profile folder: " $Folder
-                $EdgeExtensionsBase = $EdgeUserData + '\' + $Folder
-                Write-Host "Edge Profile Folder full path: " $EdgeExtensionsBase
+        foreach ($Folder in $EdgeUserDataPossibleProfiles) {
+            if ($Folder -eq "Default" -or $Folder -like "Profile*") {
+                $EdgeExtensionsBase = $EdgeUserData + '\' + $Folder + '\' + "Extensions"
+                if ($EdgeExtensionsBase | Test-Path) {
+                    $EdgeInstalledExtensions = (Get-ChildItem -path $EdgeExtensionsBase).FullName
+                    foreach ($path in $EdgeInstalledExtensions){
+                        $EdgeInstallExtensionVersions = (Get-ChildItem -Path $path).FullName | Select-Object -First 1
+                        $manifestFile = $EdgeInstallExtensionVersions + '\' + "manifest.json"
+                        if ($manifestFile | Test-Path){
+                            $manifest = Get-Content -Raw -Path $manifestFile | ConvertFrom-Json
+                            $ExtensionName = $manifest.name
+                            $ExtensionName
+                        }
+                    }
+                }
             }
         }
     }
