e17d67fecbc5a3e1756df24662ff660ab903fc4c
Scot Gabriel <scot.gabriel@gmail.com>
2022-04-28 20:10:05 -0500

    .

--- Applications/web-browsers/WebBrowser-Extensions-Get.ps1
+++ Applications/web-browsers/WebBrowser-Extensions-Get.ps1
@@ -2,19 +2,23 @@
 write-host "Operating System is on drive: " $OsDrive
 
 $UserProfiles = Get-ChildItem -Path "$OsDrive\Users"
-Write-Host "User Profiles: " $UserProfiles
+foreach ($user in $UserProfiles){
+    Write-Host "User Profile: " $user
+}
 
 $EdgeUserDataPart = "AppData\Local\Microsoft\Edge\User Data"
 
 Function EdgeBrowserProfiles($EdgeUser) {
     $EdgeUserData = ''
     [string]$EdgeUserData = $OsDrive + '\Users\' + $EdgeUser + '\' + $EdgeUserDataPart
-    Write-Host "Base Edge Path for this user: "$EdgeUserData
+    # Write-Host "Base Edge Path for this user: "$EdgeUserData
     if ($EdgeUserData | Test-Path){
         $EdgeUserDataPossibleProfiles = Get-ChildItem -Path $EdgeUserData
         foreach ($Folder in $EdgeUserDataPossibleProfiles){
             if ($Folder -eq "Default" -or $Folder -like "Profile*"){
-                Write-Host "Edge Profile folder: " $Folder
+                # Write-Host "Edge Profile folder: " $Folder
+                $EdgeExtensionsBase = $EdgeUserData + '\' + $Folder
+                Write-Host "Edge Profile Folder full path: " $EdgeExtensionsBase
             }
         }
     }
