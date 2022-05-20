2ec7934e86aedcbeeafa614245675fd4907f361d
gabe31415 <scot.gabriel@gmail.com>
2022-05-20 13:30:04 -0500

    added wifi pwd reveal

---
+++ Network/list wireless and passwords.ps1
@@ -0,0 +1,8 @@
+
+# This will list all the wifi profiles
+netsh wlan show profiles
+
+# Then pick the profile name you want to know the password to
+# for example if the profile name is "hammer" then run
+# `netsh wlan show profile name="hammer" key=clear`
+
