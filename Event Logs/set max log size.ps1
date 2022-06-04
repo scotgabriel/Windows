11ff2abfe1df441df8649bd251e894323d461e8d
Scot Gabriel <scot.gabriel@gmail.com>
2022-06-04 15:58:18 -0500

    .

--- Event Logs/set max log size.ps1
+++ Event Logs/set max log size.ps1
@@ -1,6 +1,6 @@
 # Set Maximum Event Log Size to 40MB for All Event Logs
 
-$logs = get-eventlog -list | foreach {$_.log}
+$logs = get-eventlog -list | ForEach-Object {$_.log}
 foreach ($l in $logs) {
     limit-eventlog -logname $l -MaximumSize 40MB
 }
