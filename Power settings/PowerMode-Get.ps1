19df7f7ff645815f05fdc5a4ac8b6df854bfe6c4
Scot Gabriel <scot.gabriel@gmail.com>
2022-06-21 20:19:15 -0500

    Update PowerMode-Get.ps1
    added (HP Optimized (Modern Standby))

--- Power settings/PowerMode-Get.ps1
+++ Power settings/PowerMode-Get.ps1
@@ -5,6 +5,8 @@ if($currentPowerScheme -Match "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"){
 	$localPowerMode = "Balanced"
 }elseif($currentPowerScheme -Match "a1841308-3541-4fab-bc81-f71556f20b4a"){
 	$localPowerMode = "PowerSaver"
+}elseif{$currentPowerScheme -Match "fb5220ff-7e1a-47aa-9a42-50ffbf45c673"
+	$localPowerMode = "(HP Optimized (Modern Standby))"
 }else{
 	$localPowerMode = "Unknown"
 }
