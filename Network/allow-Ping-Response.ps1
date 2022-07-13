b5a5baf2bade04c50102c25a1cf99f14a77996a8
Scot Gabriel <scot.gabriel@gmail.com>
2022-07-13 11:59:06 -0500

    Create allow-Ping-Response.ps1

---
+++ Network/allow-Ping-Response.ps1
@@ -0,0 +1,5 @@
+#IPv4
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
+
+#IPv6
+netsh advfirewall firewall add rule name="ICMP Allow incoming V6 echo request" protocol="icmpv6:8,any" dir=in action=allow
