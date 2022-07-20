182950458a653ab95968698c928b129e6f980888
Gabe31415 <scot.gabriel@gmail.com>
2022-07-19 20:39:48 -0500

    document current state

---
+++ Document Current State/Overall.ps1
@@ -0,0 +1,22 @@
+
+ipconfig /all | Out-File -FilePath C:\<path-here>
+Get-Process | Select-Object * | Out-File -FilePath C:\<path-here>
+systeminfo | Out-File -FilePath C:\<path-here>
+Get-WmiObject win32_service | Select-Object * | Out-File -FilePath C:\<path-here>
+query user | Out-File -FilePath C:\<path-here>
+reg export "HKLM" | Out-File -FilePath C:\<path-here>
+reg export "HKCU" | Out-File -FilePath C:\<path-here>
+net user | Out-File -FilePath C:\<path-here>
+net group | Out-File -FilePath C:\<path-here>
+net accounts | Out-File -FilePath C:\<path-here>
+net localgroup | Out-File -FilePath C:\<path-here>
+net share | Out-File -FilePath C:\<path-here>
+net time | Out-File -FilePath C:\<path-here>
+net view | Out-File -FilePath C:\<path-here>
+netstat -abno | Out-File -FilePath C:\<path-here>
+Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User | Format-List | Out-File -FilePath C:\<path-here>
+
+$PendingRebootStatus = Test-PendingReboot -detailed -SkipConfigurationManagerClientCheck | Select-Object *
+$PendingRebootStatus | Out-File -FilePath C:\<path-here>
+$PendingRebootFileRenames = $PendingRebootStatus | Select-Object -ExpandProperty PendingFileRenameOperationsValue
+$PendingRebootFileRenames | Out-File -Append -FilePath C:\<path-here>
